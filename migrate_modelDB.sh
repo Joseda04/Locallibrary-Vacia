088ee3005bdcb9791dccfa1a96dbb57273cbce21
Joseda04 <jtabernero9@gmail.com>
2024-01-30 17:32:00 +0100

    Prueba Superuser

--- migrate_modelDB.sh
+++ migrate_modelDB.sh
@@ -8,3 +8,8 @@ python3 manage.py makemigrations && python3 manage.py migrate
 # Colecta de staticsFiles en theme/static/
 echo "Colectando StaticFiles..."
 python3 manage.py collectstatic --noinput
+
+python "$DJANGO_MANAGE_PATH" createsuperuser --noinput \
+  --username="$DJANGO_SUPERUSER_NAME" \
+  --email="$DJANGO_SUPERUSER_EMAIL" \
+  --password="$DJANGO_SUPERUSER_PASSWORD"
